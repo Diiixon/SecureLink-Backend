4220ae75ce51ea8bea30cd401ac4544a942698c3
Diiixon <ded.tapia@duocuc.cl>
2025-10-23 16:55:50 -0300

    se corrige css de recuperar.jsx

--- src/components/Recuperar.jsx
+++ src/components/Recuperar.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import '../style/Recuperar.css'; 
+import '../style/RegisterForm.css'; 
 import RespuestaRecuperar from './RespuestaRecuperar'; 
 
 function Recuperar() { 
